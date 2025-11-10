30dd6c2536e19d4cfcb98bdfdff159bd79773224
souldbminersmwc <souldbyt@gmail.com>
2025-11-10 18:22:51 -0500

    hos-sys: remove global target (doesnt compile)
    mb

--- Source/sys-clk/sysmodule/src/clock_manager.cpp
+++ Source/sys-clk/sysmodule/src/clock_manager.cpp
@@ -281,15 +281,13 @@ void ClockManager::Tick()
         for (unsigned int module = 0; module < SysClkModule_EnumMax; module++)
         {
                 targetHz = this->context->overrideFreqs[module];
-                globalTargetHz = this->context->overrideFreqs[module];
 
                 if (!targetHz)
                 {
                     targetHz = this->config->GetAutoClockHz(this->context->applicationId, (SysClkModule)module, this->context->profile);
-                    globalTargetHz = this->config->GetAutoClockHz(SYSCLK_GLOBAL_PROFILE_TID, (SysClkModule)module, this->context->profile);
                 }
 
-                if (targetHz || globalTargetHz)
+                if (targetHz)
                 {
                     maxHz = this->GetMaxAllowedHz((SysClkModule)module, this->context->profile);
                     nearestHz = this->GetNearestHz((SysClkModule)module, targetHz, maxHz);
