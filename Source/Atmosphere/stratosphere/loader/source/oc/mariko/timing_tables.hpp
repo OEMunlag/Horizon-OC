d5bf7b0432328efd8c46fe744c2c38683d70e608
souldbminersmwc <souldbyt@gmail.com>
2025-12-11 16:10:02 -0500

    loader: format code

--- Source/Atmosphere/stratosphere/loader/source/oc/mariko/timing_tables.hpp
+++ Source/Atmosphere/stratosphere/loader/source/oc/mariko/timing_tables.hpp
@@ -17,78 +17,79 @@
 #pragma once
 #include "../mtc_timing_value.hpp"
 
-namespace ams::ldr::oc::pcv::mariko {
+namespace ams::ldr::oc::pcv::mariko
+{
 
-    struct ReplacePatch {
-        u32 freq;
-        u32 correct;
-    };
+struct ReplacePatch {
+    u32 freq;
+    u32 correct;
+};
 
-    extern const ReplacePatch  g_einput_patches[];
-    extern const u32           g_einput_patches_size;
-    const        ReplacePatch *FindEinput();
+extern const ReplacePatch g_einput_patches[];
+extern const u32 g_einput_patches_size;
+const ReplacePatch *FindEinput();
 
-    extern const ReplacePatch  g_rext_table[];
-    extern const u32           g_rext_table_size;
-    const        ReplacePatch *FindRext();
+extern const ReplacePatch g_rext_table[];
+extern const u32 g_rext_table_size;
+const ReplacePatch *FindRext();
 
-    struct AdjustPatch {
-        u32 freq;
-        s32 adjust;
-    };
+struct AdjustPatch {
+    u32 freq;
+    s32 adjust;
+};
 
-    extern const AdjustPatch g_ibdly_patches[];
-    extern const u32         g_ibdly_table_size;
-    const AdjustPatch       *FindIbdlyPatch();
+extern const AdjustPatch g_ibdly_patches[];
+extern const u32 g_ibdly_table_size;
+const AdjustPatch *FindIbdlyPatch();
 
-    extern const AdjustPatch g_obdly_patches[];
-    extern const u32         g_obdly_table_size;
-    const AdjustPatch       *FindObdlyPatch();
+extern const AdjustPatch g_obdly_patches[];
+extern const u32 g_obdly_table_size;
+const AdjustPatch *FindObdlyPatch();
 
-    extern const AdjustPatch g_tr2w_patches[];
-    extern const u32         g_tr2w_table_size;
-    const AdjustPatch       *FindTR2WPatch();
+extern const AdjustPatch g_tr2w_patches[];
+extern const u32 g_tr2w_table_size;
+const AdjustPatch *FindTR2WPatch();
 
-    extern const AdjustPatch g_quse_patches[];
-    extern const u32         g_quse_table_size;
-    const AdjustPatch       *FindQusePatch();
+extern const AdjustPatch g_quse_patches[];
+extern const u32 g_quse_table_size;
+const AdjustPatch *FindQusePatch();
 
-    extern const AdjustPatch g_qrst_patches[];
-    extern const u32         q_qrst_table_size;
-    const AdjustPatch       *FindQrstPatch();
+extern const AdjustPatch g_qrst_patches[];
+extern const u32 q_qrst_table_size;
+const AdjustPatch *FindQrstPatch();
 
-    extern const AdjustPatch g_qsafe_patches[];
-    extern const u32         g_qsafe_table_size;
-    const AdjustPatch       *FindQsafePatch();
+extern const AdjustPatch g_qsafe_patches[];
+extern const u32 g_qsafe_table_size;
+const AdjustPatch *FindQsafePatch();
 
-    extern const AdjustPatch g_pdex2rw_patches[];
-    extern const u32         g_pdex2rw_table_size;
-    const AdjustPatch       *FindPdex2rwPatch();
+extern const AdjustPatch g_pdex2rw_patches[];
+extern const u32 g_pdex2rw_table_size;
+const AdjustPatch *FindPdex2rwPatch();
 
-    extern const AdjustPatch g_cke2pden_patches[];
-    extern const u32         g_cke2pden_table_size;
-    const AdjustPatch       *FindCke2pdenPatch();
+extern const AdjustPatch g_cke2pden_patches[];
+extern const u32 g_cke2pden_table_size;
+const AdjustPatch *FindCke2pdenPatch();
 
-    struct MiscTimings {
-        u32 min_freq;
-        u32 rdv_inc;
-        u32 einput;
-        u32 quse_width;
-        s32 obdly_delta;
-    };
+struct MiscTimings {
+    u32 min_freq;
+    u32 rdv_inc;
+    u32 einput;
+    u32 quse_width;
+    s32 obdly_delta;
+};
 
-    extern const MiscTimings g_misc_table[];
-    extern const u32         g_misc_table_size;
+extern const MiscTimings g_misc_table[];
+extern const u32 g_misc_table_size;
 
-    struct FreqTW2R {
-        u32 max_freq;
-        u32 min_val;
-        u32 max_val;
-        s32 adjust;
-    };
+struct FreqTW2R {
+    u32 max_freq;
+    u32 min_val;
+    u32 max_val;
+    s32 adjust;
+};
 
-    extern const FreqTW2R  g_tw2r_table[];
-    extern const u32       g_tw2r_table_size;
-    const FreqTW2R        *FindTW2R();
+extern const FreqTW2R g_tw2r_table[];
+extern const u32 g_tw2r_table_size;
+const FreqTW2R *FindTW2R();
 
-}
+}  // namespace ams::ldr::oc::pcv::mariko
