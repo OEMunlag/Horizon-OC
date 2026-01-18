abcf11dc51fe5315aec3c9a3e158046b6a1ce907
souldbminersmwc <souldbyt@gmail.com>
2026-01-18 16:44:09 -0500

    configurator: remove

--- Source/Configurator/build.bat
+++
@@ -1,2 +0,0 @@
-python -m PyInstaller --onefile --add-data "assets;assets" --icon=assets/icon.ico --noconsole src/main.py --hidden-import=pillow --hidden-import=psutil --hidden-import=dearpygui --hidden-import=numpy --hidden-import=pathlib
-move "dist\main.exe" "dist\hocconfig.exe"
