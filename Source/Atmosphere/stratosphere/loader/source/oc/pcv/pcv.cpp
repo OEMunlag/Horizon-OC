7708e4bae6d878ea0aaa331c90cb84699c257456
souldbminersmwc <souldbyt@gmail.com>
2026-01-26 19:07:52 -0500

    Update pcv.cpp

--- Source/Atmosphere/stratosphere/loader/source/oc/pcv/pcv.cpp
+++ Source/Atmosphere/stratosphere/loader/source/oc/pcv/pcv.cpp
@@ -143,12 +143,12 @@ void SafetyCheck() {
     sValidator validators[] = {
         { C.eristaCpuBoostClock, 1020'000, 2295'000, true },
         { C.marikoCpuBoostClock, 1020'000, 2703'000, true },
-        { C.commonEmcMemVolt,    1000'000, 1350'000 }, // Official burst vmax for the RAMs is 1500mV
-        { C.eristaCpuMaxVolt,        1100,     1257 },
+        { C.commonEmcMemVolt,     912'500, 1350'000 }, // Official burst vmax for the RAMs is 1500mV
+        { C.eristaCpuMaxVolt,        1000,     1257 },
         { C.eristaEmcMaxClock,   1600'000, 2600'200 },
-        { C.marikoCpuMaxVolt,        1100,     1235 },
+        { C.marikoCpuMaxVolt,        1000,     1235 },
         { C.marikoEmcMaxClock,   1600'000, 3500'000 },
-        { C.marikoEmcVddqVolt,    400'000,  700'000 },
+        { C.marikoEmcVddqVolt,    250'000,  700'000 },
         { eristaCpuDvfsMaxFreq,  1785'000, 2295'000 },
         { marikoCpuDvfsMaxFreq,  1785'000, 2703'000 },
         { eristaGpuDvfsMaxFreq,   768'000, 1152'000 },
