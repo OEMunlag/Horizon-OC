92f378a80f1e7415e250f12752906785c17e2a26
Souldbminer <162390887+souldbminerr@users.noreply.github.com>
2026-02-09 18:59:09 -0500

    fix svfs again

--- Source/Atmosphere/stratosphere/loader/source/oc/pcv/pcv_mariko.cpp
+++ Source/Atmosphere/stratosphere/loader/source/oc/pcv/pcv_mariko.cpp
@@ -102,7 +102,7 @@ namespace ams::ldr::hoc::pcv::mariko {
         }
 
         /* C.marikoGpuVmin is non zero, user sets manual voltage. */
-        if (!C.marikoGpuVmin || C.marikoGpuVmin == 1) {
+        if (C.marikoGpuVmin != 0 && C.marikoGpuVmin != 1) {
             PATCH_OFFSET(ptr, C.marikoGpuVmin);
             R_SUCCEED();
         }
